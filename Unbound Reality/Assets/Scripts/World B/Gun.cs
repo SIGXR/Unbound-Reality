a94166bdbca40b3a465bdcd9e20dba7d2a902ac2
SIGXR <55867142+SIGXR@users.noreply.github.com>
2019-10-31 19:34:34 -0400

    Gun
    Gun

---
+++ Unbound Reality/Assets/Scripts/World B/Gun.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+
+    public int maxAmmo = 12;
+    private int currentAmmo;
+    public float reloadTime= 1.5f;
+    private bool isReloading = false;
+    void Start()
+    {
+        if(currentAmmo == -1)
+        currentAmmo = maxAmmo;
+    }
+    void Update()
+    {
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        yield return new WaitForSeconds(reloadTime);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+    void Shoot()
+    {
+        Debug.Log("I shot you...");
+        currentAmmo--;
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage();
+            }
+        }
+    }
+}
---
+++ Unbound Reality/Assets/Target.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public void TakeDamage()
+    {
+        Debug.Log("I got hit!");
+        var cubeRenderer = gameObject.GetComponent<Renderer>();
+        cubeRenderer.material.SetColor("_Color", Color.red);
+    }
+}
