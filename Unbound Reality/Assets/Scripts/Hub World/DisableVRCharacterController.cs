ab60572aa8eda8b3e7fa20f6d3db0b13d5fe3bff
SIGXR <55867142+SIGXR@users.noreply.github.com>
2019-11-05 21:02:17 -0500

    Made it so you can move when there's no HMD
    Jank as hell, you'll be sent flying for some reason. Someone help

---
+++ Unbound Reality/Assets/Scripts/Hub World/DisableVRCharacterController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class DisableVRCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    public Camera nonVRCamera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (!XRDevice.isPresent)
+        {
+            Debug.Log("I couldn't find a HMD!!!");
+            this.gameObject.GetComponent<OVRPlayerController>().enabled = false;
+            this.gameObject.GetComponent<OVRSceneSampleController>().enabled = false;
+            this.gameObject.GetComponent<OVRDebugInfo>().enabled = false;
+            this.gameObject.GetComponent<CharacterCameraConstraint>().enabled = false;
+            nonVRCamera.enabled = false;
+        }
+        else
+        {
+            Debug.Log("I found a HMD!!!");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(0,0,-1 * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(0, 0, 1 * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(-1 * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(1 * Time.deltaTime, 0, 0));
+        }
+    }
+}
