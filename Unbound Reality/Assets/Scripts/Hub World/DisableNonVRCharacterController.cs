5550ce424fe469ef2922d0125477b2d2a2d1be46
SIGXR <55867142+SIGXR@users.noreply.github.com>
2019-11-05 21:18:17 -0500

    Different Approach for NonVR Character Controller
    No Headset Controller

---
+++ Unbound Reality/Assets/Scripts/Hub World/DisableNonVRCharacterController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class DisableNonVRCharacterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (XRDevice.isPresent)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Unbound Reality/Assets/Scripts/Hub World/NonVRCharacterController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonVRCharacterController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        Gizmos.DrawRay(transform.position, Vector3.forward);
+    }
+}
