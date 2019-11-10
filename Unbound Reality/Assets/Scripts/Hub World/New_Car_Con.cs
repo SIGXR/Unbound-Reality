3fadceee2753b1c6d4aefee68fcc544b594e4345
SIGXR <55867142+SIGXR@users.noreply.github.com>
2019-11-10 01:07:49 -0500

    Fixed Car Somewhat
    Fixed Car Somewhat

--- Unbound Reality/Assets/Scripts/Hub World/New_Car_Con.cs
+++ Unbound Reality/Assets/Scripts/Hub World/New_Car_Con.cs
@@ -4,46 +4,57 @@ using UnityEngine;
 
 public class New_Car_Con : MonoBehaviour
 {
-    public float MotorForce, SteerForce, BrakeForce;
+    public float m_horizontalInput, m_verticalInput, m_steeringAngle;
+    public float maxSteerAngle = 30;
+    public float motorForce, steerForce, brakeForce;
     public WheelCollider FR_L_Wheel, FR_R_Wheel, RE_L_Wheel, RE_R_Wheel;
-
-    private void Start()
-    {
-        
-    }
 
     private void Update()
     {
-        float v = Input.GetAxis("Vertical") * MotorForce;
-        float h = Input.GetAxis("Horizontal") * SteerForce;
+        m_verticalInput = Input.GetAxis("Vertical");
+        m_horizontalInput = Input.GetAxis("Horizontal");
+        Accelerate();
+        Steer();
+        Brake();
+    }
 
-       RE_L_Wheel.motorTorque = v;
-       RE_R_Wheel.motorTorque = v;
+    private void Accelerate()
+    {
+        FR_L_Wheel.motorTorque = m_verticalInput * motorForce;
+        FR_R_Wheel.motorTorque = m_verticalInput * motorForce;
+        RE_L_Wheel.motorTorque = m_verticalInput * motorForce;
+        RE_R_Wheel.motorTorque = m_verticalInput * motorForce;
+    }
 
-       FR_L_Wheel.motorTorque = h;
-       FR_R_Wheel.motorTorque = h;
+    private void Steer()
+    {
+        m_steeringAngle = maxSteerAngle * m_horizontalInput;
+        FR_L_Wheel.steerAngle = m_steeringAngle;
+        FR_R_Wheel.steerAngle = m_steeringAngle;
+    }
 
+    private void Brake()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
-            RE_L_Wheel.brakeTorque = BrakeForce;
-            RE_R_Wheel.brakeTorque = BrakeForce;
+            Debug.Log("Breaking");
+            RE_L_Wheel.brakeTorque = brakeForce;
+            RE_R_Wheel.brakeTorque = brakeForce;
         }
-
-        if (Input.GetKeyUp(KeyCode.Space))
+        else if (Input.GetKeyUp(KeyCode.Space))
         {
             RE_L_Wheel.brakeTorque = 0;
             RE_R_Wheel.brakeTorque = 0;
         }
-
-        if (Input.GetAxis("Vertical") == 0)
-        {
-           RE_L_Wheel.brakeTorque = BrakeForce;
-           RE_R_Wheel.brakeTorque = BrakeForce;
-        }
-        else
-        {
-           RE_L_Wheel.brakeTorque = 0;
-           RE_R_Wheel.brakeTorque = 0;
-        }
+        //if (m_horizontalInput == 0)
+        //{
+        //    RE_L_Wheel.brakeTorque = BrakeForce;
+        //    RE_R_Wheel.brakeTorque = BrakeForce;
+        //}
+        //else
+        //{
+        //RE_L_Wheel.brakeTorque = 0;
+        //RE_R_Wheel.brakeTorque = 0;
+        //}
     }
 }
