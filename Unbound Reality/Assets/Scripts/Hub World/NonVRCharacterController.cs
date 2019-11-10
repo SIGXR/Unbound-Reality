51ad265d6160cdb26b7d4506673b2ea34e78ee8d
SIGXR <55867142+SIGXR@users.noreply.github.com>
2019-11-10 01:42:29 -0500

    Rewrote the non VR Controller
    Done

--- Unbound Reality/Assets/Scripts/Hub World/NonVRCharacterController.cs
+++ Unbound Reality/Assets/Scripts/Hub World/NonVRCharacterController.cs
@@ -6,20 +6,15 @@ public class NonVRCharacterController : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public float turnSpeed = 50f;
+
+    private float verticalAxis, horizontalAxis;
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        verticalAxis = Input.GetAxis("Vertical");
+        horizontalAxis = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime * verticalAxis);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalAxis);
     }
 
     private void OnTriggerEnter(Collider other)
