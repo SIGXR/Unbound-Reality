f5aa155677b56034499598fd2f48704b4dc3078c
Dadarkforce <Al.Jamalul@gmail.com>
2019-11-17 12:33:12 -0500

    Car, Portal and Reorganized

---
+++ Unbound Reality/Assets/Hub World/Scripts/Non-VR Charater Controller/NonVRCharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonVRCharacterController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+
+    private float verticalAxis, horizontalAxis;
+	
+	// Update is called once per frame
+	void Update () {
+        verticalAxis = Input.GetAxis("Vertical");
+        horizontalAxis = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime * verticalAxis);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalAxis);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("DodgeModel") || other.name.Equals("DodgeModifiersApplied_AddedInterior_11.16.19"))
+        {
+            this.gameObject.SetActive(false);
+            other.GetComponentInChildren<Camera>().enabled = true;
+            other.GetComponent<New_Car_Con>().enabled = true;
+        }
+    }
+}
