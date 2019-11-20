d78ba5bbae1dde2a4c6d80e31b4c1f805a80ec3a
Dadarkforce <Al.Jamalul@gmail.com>
2019-11-19 21:14:04 -0500

    Waterfall

---
+++ Unbound Reality/Assets/Hub World/Models, Shaders & Textures/Waterfall/Waterfall_Shader.shader
@@ -0,0 +1,62 @@
+﻿Shader "Unlit/Waterfall_Shader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		// Alpha CutOff
+		_Cutoff ("Alpha Cutoff", Range(0.0, 1.0))=0.5
+	}
+	SubShader
+	{
+		Tags { "Queue"="Geometry" }
+
+		Pass
+		{
+			// Enables transparency for material
+			Blend SrcAlpha OneMinusSrcAlpha
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float _Cutoff;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				//o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				//Panning
+				float2 distuv = float2(i.uv.x, i.uv.y + _Time.x * 20);
+				float4 color = tex2D(_MainTex, distuv);
+
+				//Alpha Cutoff, Note: step is an if statement for shaders which returns 0 or 1
+				color.a = step(0.0, i.uv.y-_Cutoff);
+
+				return color;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Unbound Reality/Assets/Hub World/Scripts/Waterfall/Waterfall.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waterfall : MonoBehaviour
+{
+    public Transform _ParticleSystem;
+
+    private MeshRenderer _MeshRenderer;
+
+    private void Awake()
+    {
+        _MeshRenderer = GetComponent<MeshRenderer>();
+    }
+    //Allows updating position of the waterfall shader
+    private void OnTriggerStay(Collider other)
+    {
+        UpdateStream(GetHeight(other));
+    }
+
+    //Resets the position of the waterfall shader
+    private void OnTriggerExit(Collider other)
+    {
+        UpdateStream(0);
+    }
+
+    //Adds up to half the collider's y height to the waterfall. Bare in mind this changes based on the collided object's pivot location.
+    private float GetHeight(Collider collider)
+    {
+        return collider.transform.position.y + collider.bounds.size.y/2;
+    }
+
+    private void UpdateStream(float newHeight)
+    {
+        //Particle
+        Vector3 newPosition = new Vector3(transform.position.x, newHeight, transform.position.z);
+        _ParticleSystem.position = newPosition;
+
+        //Height cutoff, namely accounts for any y-scaling that's hapepning in the scene
+        newHeight /= transform.localScale.y;
+
+        Debug.Log(newHeight);
+        _MeshRenderer.material.SetFloat("_Cutoff", newHeight);
+    }
+}
