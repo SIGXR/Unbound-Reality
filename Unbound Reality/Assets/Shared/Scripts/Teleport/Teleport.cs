0e10cf28cf52c8493e1ab763eec5364d7c6177d7
JustPlainDark <JustPlainDark@users.noreply.github.com>
2019-12-11 18:54:37 -0500

    Portal Changes (Prompt)

--- Unbound Reality/Assets/Shared/Scripts/Teleport/Teleport.cs
+++ Unbound Reality/Assets/Shared/Scripts/Teleport/Teleport.cs
@@ -2,24 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class Teleport : MonoBehaviour {
+public class Teleport : MonoBehaviour
+{
 
     public string areaName;
     public Animator fade;
+    public Text bottomText;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            fade.SetTrigger("FadeOut");
-            StartCoroutine(SceneChange());
+            Debug.Log("Wheeeeeee");
+            bottomText.enabled = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                fade.SetTrigger("FadeOut");
+                StartCoroutine(SceneChange());
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            bottomText.enabled = false;
         }
     }
 
     IEnumerator SceneChange()
     {
         yield return new WaitForSeconds(1);
-        SceneManager.LoadScene(areaName,LoadSceneMode.Single);
+        SceneManager.LoadScene(areaName, LoadSceneMode.Single);
     }
 }
