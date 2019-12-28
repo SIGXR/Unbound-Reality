22ed6395a300479b9592a8186372b6626ed66b0d
JoshA2000 <56370202+JoshA2000@users.noreply.github.com>
2019-12-28 08:52:21 -0500

    Class system
    Before starting the game, you can choose 4 different classes. At the moment, only warrior and mage work. The difference between classes are speed and physical attack.

---
+++ Unbound Reality/Assets/Hub World/Scripts/Class Screen/BaseClass.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseClass {
+    private string characterClassName;
+    private string characterClassDescription;
+
+    private float speed;
+    private float strength;
+    private bool status;
+
+    public string CharacterClassName
+    {
+        get { return characterClassName; }
+        set { characterClassName = value; }
+    }
+
+    public string CharacterClassDescription
+    {
+        get { return characterClassDescription; }
+        set { characterClassDescription = value; }
+    }
+
+    public float Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+
+    public float Strength
+    {
+        get { return strength; }
+        set { strength = value; }
+    }
+
+    public bool Status
+    {
+        get { return status; }
+        set { status = value; }
+    }
+}
---
+++ Unbound Reality/Assets/Hub World/Scripts/Class Screen/Class_Select.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Class_Select : MonoBehaviour
+{
+    public Warrior w = new Warrior();
+    public Mage m = new Mage();
+
+    public void PlayGame(Button btn)
+    {
+        float getMove = NonVRCharacterController.moveSpeed;
+        SceneManager.LoadScene("Hub World");
+
+        if (btn.name == "WarriorButton")
+        {
+            float move = w.Speed();
+            NonVRCharacterController.moveSpeed = move;
+        }
+        else if (btn.name == "MageButton")
+        {
+            float move = m.Speed();
+            NonVRCharacterController.moveSpeed = move;
+        }
+        Debug.Log(btn.name);
+    }
+}
