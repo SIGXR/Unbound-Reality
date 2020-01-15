d3b72329e5ea3ea29d9b692c7e27d2a96b0efa2c
Anthony Rios <ar664@njit.edu>
2020-01-15 06:30:29 -0500

    Alpha PUN + chat system

---
+++ Unbound Reality/Assets/Shared/Scripts/Network (PUN)/GameSetupController.cs
@@ -0,0 +1,46 @@
+﻿using Photon.Pun;
+using System.IO;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class GameSetupController : MonoBehaviour {
+
+    private GameObject localPlayer;
+    [SerializeField]
+    private GameObject spawnPoint;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if(localPlayer == null)
+        {
+            CreatePlayer();
+        }
+	}
+
+    //Creates a networked player object for each player that loads into the multiplayer room
+    private void CreatePlayer()
+    {
+        Vector3 spawnPosition;
+        Debug.Log("Creating Player");
+
+        if(spawnPoint == null)
+        {
+            spawnPosition = Vector3.zero;
+        } else {
+            spawnPosition = spawnPoint.transform.position;
+        }
+
+        localPlayer = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Non VR Character Controller"), spawnPosition, Quaternion.identity);
+        
+        if(XRDevice.isPresent)
+        {
+            //Load player name from VRCharacterController
+        } else
+        {
+            NonVRCharacterController nvcc = localPlayer.GetComponent<NonVRCharacterController>();
+            nvcc.PlayerName = PlayerPrefs.GetString("Name", "Dummy");
+        }
+
+    }
+}
---
+++ Unbound Reality/Assets/Shared/Scripts/Network (PUN)/QuickStartRoomController.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+//Use a callback function in this script which is called by QuickStartLobbyController.cs 
+public class QuickStartRoomController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private int multiplayerSceneIndex;
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    //Callback function for when we succesfully join or create a room
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+        StartGame();
+    }
+
+    //Function for loading into the multiplayer scene
+    private void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting Game");
+            //Because we set the AutoSyncScene... all players who joins this room after mastesrclient has loaded multiplayerScene will also be loaded into said scene.
+            PhotonNetwork.LoadLevel(multiplayerSceneIndex);
+        }
+    }
+}
