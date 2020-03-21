04bc8dd7331f99318c0ff9db7d6da4d4a805e609
JoshA2000 <56370202+JoshA2000@users.noreply.github.com>
2020-03-21 10:43:40 -0400

    Added in Enemy Spawner

---
+++ Unbound Reality/Assets/Hub World/Scripts/Slime/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public List<GameObject> enemies;
+    public float spawnTime = 5f;
+    public Transform[] spawnPoints;
+    public int maxEnemies = 10;
+    private int enemyCounter = 0;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject slime in enemies)
+        {
+            if (slime == null)
+            {
+                enemies.Remove(slime);
+            }
+        }
+    }
+
+    public void Spawn()
+    {
+        enemyCounter = enemies.Count;
+        if (enemyCounter < maxEnemies)
+        {
+            GameObject clone = (GameObject)Instantiate(enemy, transform.position, transform.rotation);
+            enemies.Add(clone);
+        }
+        Debug.Log(enemyCounter);
+    }
+
+
+}
